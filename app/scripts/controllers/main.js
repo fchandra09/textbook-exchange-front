5b30991490e3461d197d84a84f623afbddaf1bfc
Mike Kreiser <mkreise2@illinois.edu>
2016-11-28 14:22:26 -0600

    Yeoman scaffold complete

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name textbookExchangeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the textbookExchangeApp
+ */
+angular.module('textbookExchangeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
