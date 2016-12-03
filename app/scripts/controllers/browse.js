ed3f9725d96cab5dfa8348ab14859adeb7beb099
Mike Kreiser <mkreise2@illinois.edu>
2016-12-02 20:29:04 -0600

    Add Browse page

--- app/scripts/controllers/browse.js
+++ app/scripts/controllers/browse.js
@@ -2,17 +2,32 @@
 
 angular.module('textbookExchangeApp')
 
-.controller('BrowseCtrl', function() {
+.controller('BrowseCtrl', function($scope) {
   var page = 1;
   var limit = 20;
 
-  Posts.getAll({'limit': limit, 'skip': page * limit}).then(function(response) {
-    $scope.posts = response.data.data;
+  // Posts.getAll({'limit': limit, 'skip': page * limit}).then(function(response) {
+  //   $scope.posts = response.data.data;
 
-    angular.forEach($scope.posts, function(post) {
-      Books.get(post.bookId).then(function(response) {
-        post.book = response.data.data;
-      });
-    });
-  });
+  //   angular.forEach($scope.posts, function(post) {
+  //     Books.get(post.bookId).then(function(response) {
+  //       post.book = response.data.data;
+  //     });
+  //   });
+  // });
+
+  $scope.posts = [{
+    condition: 'New',
+    trades: ['Some book'],
+    price: 1234.56,
+    bookId: 'some id',
+    sellerId: 'some id',
+    dateCreated: new Date(),
+    book: {
+      authors: ['some author', 'some Author 2'],
+      classes: ['some classes'],
+      coverImage: 'images/books/978-0-06-050577-6.jpg',
+      title: 'This Book is Lit'
+    }
+  }];
 });
