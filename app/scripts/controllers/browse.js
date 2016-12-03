16c3732362196f4dfb0eaa15ff9bd1fe1dcd85cb
Mike Kreiser <mkreise2@illinois.edu>
2016-12-02 19:12:57 -0600

    Doing posts stuff

--- app/scripts/controllers/browse.js
+++ app/scripts/controllers/browse.js
@@ -3,5 +3,16 @@
 angular.module('textbookExchangeApp')
 
 .controller('BrowseCtrl', function() {
+  var page = 1;
+  var limit = 20;
 
+  Posts.getAll({'limit': limit, 'skip': page * limit}).then(function(response) {
+    $scope.posts = response.data.data;
+
+    angular.forEach($scope.posts, function(post) {
+      Books.get(post.bookId).then(function(response) {
+        post.book = response.data.data;
+      });
+    });
+  });
 });
